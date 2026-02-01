2e51670d47e142dfb44f0b1e18281623b52be0ae
Romell <aatroxlunag0w0@gmail.com>
2026-01-31 23:46:14 -0600

    first commit

---
+++ detalle.js
@@ -0,0 +1,28 @@
+const contenedorDetalle = document.getElementById("detalleProducto")
+
+let parametrosURL = new URLSearchParams(window.location.search)
+let idProducto = parametrosURL.get("id")
+
+fetch("https://dummyjson.com/products/" + idProducto)
+.then(respuesta => respuesta.json())
+.then(producto => {
+    let opiniones = ""
+    producto.reviews.forEach(opinion => {
+        opiniones += `<div>
+                <p><strong>Usuario:</strong> ${opinion.reviewerName}</p>
+                <p><strong>Comentario:</strong> ${opinion.comment}</p>
+                <p><strong>Calificación:</strong> ${opinion.rating} / 5</p><hr></div>`
+    })
+
+    contenedorDetalle.innerHTML = `
+        <h2>${producto.title}</h2>
+        <img src="${producto.thumbnail}" style="width:300px">
+        <p><strong>Descripción:</strong> ${producto.description}</p>
+        <p><strong>Precio:</strong> $${producto.price}</p>
+        <p><strong>Marca:</strong> ${producto.brand}</p>
+        <p><strong>Categoría:</strong> ${producto.category}</p>
+        <p><strong>Rating promedio:</strong> ${producto.rating}</p>
+        <p><strong>Stock:</strong> ${producto.stock}</p>
+        <h3>Opiniones</h3>
+        ${opiniones}`
+})
---
+++ main.js
@@ -0,0 +1,53 @@
+const contenedorProductos = document.getElementById("contenedorProductos")
+const inputBusqueda = document.getElementById("inputBusqueda")
+const botonBuscar = document.getElementById("botonBuscar")
+
+let listaProductos = []
+
+//cargar 10 productos al iniciar
+fetch("https://dummyjson.com/products?limit=10")
+.then(respuesta => respuesta.json())
+.then(datos => {
+    listaProductos = datos.products
+    mostrarProductos(listaProductos)
+})
+
+//Mostrar productos
+const mostrarProductos = (productos) => {
+    contenedorProductos.innerHTML = ""
+
+    productos.forEach(producto => {
+        contenedorProductos.innerHTML += `<div class="tarjeta-producto" onclick="verDetalle(${producto.id})">
+                <img src="${producto.thumbnail}" class="imagen-producto">
+                <div class="info-producto">
+                    <div class="nombre-producto">${producto.title}</div>
+                    <div class="categoria-producto">${producto.category}</div>
+                    <div class="precio-producto">$${producto.price}</div>
+                    <div>Rating: ${producto.rating}</div>
+                </div>
+            </div>`
+    })
+}
+
+//buscar productos
+botonBuscar.addEventListener("click", () => {
+    let textoBusqueda = inputBusqueda.value
+
+    if(textoBusqueda === ""){
+        fetch("https://dummyjson.com/products?")
+        .then(respuesta => respuesta.json())
+        .then(datos => mostrarProductos(datos.products))
+        return
+    }
+
+    fetch("https://dummyjson.com/products/search?q=" + textoBusqueda)
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+        mostrarProductos(datos.products)
+    })
+})
+
+// ir a detalle
+const verDetalle = (idProducto) => {
+    window.location.href = "detalles.html?id=" + idProducto
+}
